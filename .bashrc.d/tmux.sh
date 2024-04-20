b8127a24eba9ea07c7cc72bc5d8ef1c44f9e5693
Tyler Wayne <tylerwayne3@gmail.com>
2024-04-19 22:45:18 -0700

    turn one line functions into aliases. add one to close neighboring window without moving panes

--- .bashrc.d/tmux.sh
+++ .bashrc.d/tmux.sh
@@ -2,10 +2,6 @@
 # Shell commands for use with tmux
 
 # these are named to align with vim naming conventions
-function vsplit() {
-  tmux split-window -h "$@"
-}
-
-function hsplit() {
-  tmux split-window -v "$@"
-}
+alias vsplit="tmux split-window -h"
+alias hsplit="tmux split-window -v"
+alias close="tmux kill-pane -t"
