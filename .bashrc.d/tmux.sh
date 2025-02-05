c231b1b6a66b26319b5bb00497071417f0d6914d
Tyler Wayne <tylerwayne3@gmail.com>
2025-02-05 00:17:22 -0800

    enclose tmux aliases in conditional that checks $TMUX

--- .bashrc.d/tmux.sh
+++ .bashrc.d/tmux.sh
@@ -1,7 +1,11 @@
 # .tmux.sh
 # Shell commands for use with tmux
 
-# these are named to align with vim naming conventions
-alias vsplit="tmux split-window -h"
-alias hsplit="tmux split-window -v"
-alias close="tmux kill-pane -t"
+if [ -n "$TMUX" ]; then
+
+  # these are named to align with vim naming conventions
+  alias vsplit="tmux split-window -h"
+  alias hsplit="tmux split-window -v"
+  alias close="tmux kill-pane -t"
+
+fi
